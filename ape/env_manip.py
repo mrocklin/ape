340d89f5964657b1777e82b03a03fcc8c727aba1
Matthew Rocklin <mrocklin@gmail.com>
2012-05-25 12:27:52 -0500

    added docstrings to env_manip functions

--- ape/env_manip.py
+++ ape/env_manip.py
@@ -3,6 +3,14 @@ import theano.tensor as T
 import cPickle
 
 def math_optimize(env):
+    """
+    Creates a new Env that has been optimized using no gpu or inplace operations
+
+    Does not overwrite input
+
+    >>> env2 = math_optimize(env)
+    """
+
     fast_run_no_inplace = theano.compile.optdb.query(
         theano.gof.Query(include = ['fast_run'], exclude = ['inplace', 'gpu']))
     optimized_env = env.clone()
@@ -10,11 +18,35 @@ def math_optimize(env):
     return optimized_env
 
 def pack(env):
+    """
+    Pickles a theano.Env into a string
+
+    >>> import theano
+    >>> import cPickle
+    >>> from ape.env_manip import pack, unpack
+    >>> x = theano.tensor.matrix('x')
+    >>> y = x+x
+    >>> env = theano.Env([x], [y])
+    >>> type(pack(env))
+    str
+    >>> str(unpack(pack(env))) == str(env)
+    True
+
+    See Also:
+        unpack
+    """
+
     ins, outs  = theano.gof.graph.clone(env.inputs, env.outputs)
     s = cPickle.dumps((ins, outs))
     return s
 
 def unpack(s):
+    """
+    Unpickle a packed string into an Env
+
+    See Also:
+        pack
+    """
     ins, outs = cPickle.loads(s)
     env = theano.Env(ins, outs)
     return env
