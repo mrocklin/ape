8a47dd841642c5d4270cbd3c177bdebad54828a0
Matthew Rocklin <mrocklin@gmail.com>
2012-06-01 13:27:48 -0500

    added some documentation for gen_code

--- ape/schedule.py
+++ ape/schedule.py
@@ -47,6 +47,20 @@ def machine_dict_to_code(d):
         for machine in d]))
 
 def gen_code(sched, env_filename, var_shapes):
+    """
+    Generates all of the code specific to this schedule instance
+
+    Returns a dictionary with code for compilation, instantiation, computation,
+    etc... of the schedule.
+
+    This dictionary can be put into the template.py file i.e.
+
+    >>> d = gen_code(sched, "env.dat", {'x': (5,5), 'y':(5,5)})
+    >>> generic_code = open('template.py').read()
+    >>> code = generic_code%d
+    >>> open('result.py', 'w').write(code)
+
+    """
     env_file = open(env_filename, 'w')
 
     jobs_of, job_runs_on, var_stored_on, var_needed_on = useful_dicts(sched)
@@ -67,11 +81,16 @@ def gen_code(sched, env_filename, var_shapes):
     pack_many(envs, env_file)
     env_file.close()
 
+    # Creates code like
+    # link = mode.linker.accept(envs[4])
+    # fn_4 = link.make_function()
     compile_code = {machine : sum([["link = mode.linker.accept(envs[%d])"%i,
                                    "fn_%d = link.make_function()"%i]
                                    for i in map(job_id, jobs_of[machine])], [])
                                    for machine in machines}
 
+    # Creates code like
+    # var1 = np.empty((5, 5), dtype="float64")
     var_init_code = {machine :
            ["%s = np.empty(%s, dtype='%s')"%(
                                         vid, var_shapes[vid], var_dtype[vid])
@@ -79,6 +98,8 @@ def gen_code(sched, env_filename, var_shapes):
                 if machine in var_needed_on[vid]]
             for machine in machines}
 
+    # Creates code like
+    # recv(var1, 13, "ankaa")
     recv_code = {machine:
             ["recv(%s, %s, '%s')"%(vid, variable_tags[vid], var_stored_on[vid])
                 for job in jobs_of[machine]
@@ -87,6 +108,11 @@ def gen_code(sched, env_filename, var_shapes):
                 and var_stored_on[vid] != machine]
             for machine in machines}
 
+    # Creates code like
+    # wait_on_recv(13, "ankaa")
+    # var1, var2 = fn_15(var5, var3)
+    # send(var1, 15, "mimosa")
+    # send(var2, 18, "arroyitos")
     compute_code = {machine:
         sum([["wait_on_recv(%s,'%s')"%(variable_tags[vid], var_stored_on[vid])
                 for vid, var in zip(map(var_id, job.inputs), job.inputs)
@@ -105,6 +131,8 @@ def gen_code(sched, env_filename, var_shapes):
             for job in jobs_of[machine]], [])
         for machine in machines}
 
+    # Actually, the above objects are dicts that contain lines of code.
+    # lets turn that into blocks of code branched on machine id
     var_init_string = machine_dict_to_code(var_init_code)
     compile_string  = machine_dict_to_code(compile_code)
     recv_string     = machine_dict_to_code(recv_code)
