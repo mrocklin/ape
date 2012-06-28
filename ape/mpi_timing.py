58acb3a5e4697cd03070f7e69b34a22700e63d39
Matthew Rocklin <mrocklin@gmail.com>
2012-06-28 10:15:31 -0500

    mpi_timing.py now includes pure python function

--- ape/mpi_timing.py
+++ ape/mpi_timing.py
@@ -1,29 +1,45 @@
-from mpi_prelude import *
-import time
-from sys import argv, stdout
-import ast
 
-ns, sender, receiver = argv[1:]
-ns = ast.literal_eval(ns)
+if __name__ == '__main__':
+    from mpi_prelude import *
+    import time
+    from sys import argv, stdout
+    import ast
 
-results = []
+    ns, sender, receiver = argv[1:]
+    ns = ast.literal_eval(ns)
 
-for n in ns:
-    x = np.ones((n,n), dtype=np.float32)
+    results = []
+
+    for n in ns:
+        x = np.ones(n, dtype=np.float32)
+
+        if host == receiver:
+            recv(x, n, sender)
+
+        comm.barrier()
+        if host == receiver:
+            starttime = time.time()
+            wait(n)
+            endtime = time.time()
+            duration = endtime - starttime
+            results.append((x.nbytes, duration))
+
+        if host == sender:
+            send(x, n, receiver)
 
     if host == receiver:
-        recv(x, n, sender)
+        stdout.write("[%s]\n"%(',\n'.join(map(str, results))))
 
-    comm.barrier()
-    if host == receiver:
-        starttime = time.time()
-        wait(n)
-        endtime = time.time()
-        duration = endtime - starttime
-        results.append((x.nbytes, duration))
+else:
+    import ast
+    import os
+    def comm_times(ns, send_host, recv_host):
 
-    if host == sender:
-        send(x, n, receiver)
+        hosts = (send_host, recv_host)
+        file = open('_machinefile.txt', 'w')
+        file.write('\n'.join(hosts))
+        file.close()
+        s = os.popen('''mpiexec -np 2 -machinefile _machinefile.txt python mpi_timing.py "%s" %s %s'''%(ns, hosts[0], hosts[1]))
 
-if host == receiver:
-    stdout.write("[%s]\n"%(',\n'.join(map(str, results))))
+        values = ast.literal_eval(s.read())
+        return values
--- ape/test/test_mpi_timing.py
+++ ape/test/test_mpi_timing.py
@@ -1,17 +1,9 @@
 import ast
 import os
+from ape.mpi_timing import comm_times
 
 def test_mpi_timing():
-    hosts = ['ankaa.cs.uchicago.edu', 'mimosa.cs.uchicago.edu']
-
-    file = open('_machinefile.txt', 'w')
-    file.write('\n'.join(hosts))
-    file.close()
-
-    ns = [10, 20, 50, 100, 1000]
-
-    s = os.popen('''mpiexec -np 2 -machinefile _machinefile.txt python mpi_timing.py "%s" %s %s'''%(ns, hosts[0], hosts[1]))
-
-    values = ast.literal_eval(s.read())
+    values = comm_times([10, 100, 1000], 'ankaa.cs.uchicago.edu',
+            'mimosa.cs.uchicago.edu')
     assert all(isinstance(n, int) for n,time in values)
     assert all(isinstance(time, float) for n,time in values)
