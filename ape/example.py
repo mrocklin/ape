7e82fb99125fe073c145af3f51247566e8d44eb6
Matthew Rocklin <mrocklin@gmail.com>
2012-07-03 14:09:53 -0500

    adding commtimes to example
    does not yet work

--- ape/example.py
+++ ape/example.py
@@ -2,8 +2,7 @@ import theano
 from theano_to_milp import (make_ilp, dummy_compute_cost, dummy_comm_cost,
         dummy_ability, compute_schedule)
 from master import compile
-from timings import (load_dict, make_runtime_function, compute_runtimes,
-        save_dict)
+from timings import (load_dict, make_runtime_function, make_commtime_function, save_dict)
 from env_manip import variables_with_names
 
 x = theano.tensor.matrix('x')
@@ -11,11 +10,15 @@ y = theano.tensor.matrix('y')
 z = theano.tensor.dot(x, x) + y[:,0].sum() - x*y
 variables_with_names([x,y], [z]) # give names to all variables between x,y and z
 
-times = load_dict('times.dat')
-compute_cost = make_runtime_function(times)
-
 env = theano.Env([x, y], [z])
-input_shapes ={x:(5,5), y:(5,5)}
+input_shapes = {x:(5,5), y:(5,5)}
+
+compute_times = load_dict('compute_times.dat')
+compute_cost = make_runtime_function(compute_times)
+
+comm_times = load_dict('comm_times.dat')
+comm_cost = make_commtime_function(comm_times, input_shapes)
+
 machine_ids = ["ankaa.cs.uchicago.edu", "mimosa.cs.uchicago.edu"]
 
 code = compile(env, machine_ids, compute_cost, dummy_comm_cost,
--- ape/example_times.py
+++ ape/example_times.py
@@ -1,6 +1,7 @@
 import theano
-from timings import compute_runtimes, save_dict
+from timings import compute_runtimes, compute_commtimes
 from env_manip import variables_with_names
+from util import save_dict
 
 x = theano.tensor.matrix('x')
 y = theano.tensor.matrix('y')
@@ -10,4 +11,8 @@ variables_with_names([x,y], [z]) # give names to all variables between x,y and z
 
 an_times = compute_runtimes([x,y], [z], {x:(1000,1000), y:(1000,1000)})
 times = {('ankaa.cs.uchicago.edu','mimosa.cs.uchicago.edu'):an_times}
-save_dict('times.dat', times)
+save_dict('compute_times.dat', times)
+
+comm_dict = compute_commtimes([10,100,1000,2000]*5,
+            {'ankaa.cs.uchicago.edu', 'mimosa.cs.uchicago.edu'}))
+save_dict('comm_times.dat', comm_dict)
