8aefe3873061163b6bc5fe922f2db5c904434f86
Matthew Rocklin <mrocklin@gmail.com>
2012-08-27 16:26:47 -0500

    computation/comm functions more robust
    known_shapes dict now takes strings internally
    
    commtime function returns zero if sender == receiver

--- ape/timings.py
+++ ape/timings.py
@@ -139,14 +139,17 @@ def make_commtime_function(cdict, known_shapes):
     """
 
     bytes_fn = mpi_timing.function_from_group_dict(cdict)
+    known_shapes = {str(key): known_shapes[key] for key in known_shapes}
 
     def bytes(var):
         """ Compute the bytes that a theano variable holds """
-        shape = known_shapes[var]
+        shape = known_shapes[str(var)]
         return prod(shape)*bytes_of_dtype(var.dtype)
 
     def commtime(an, sender, receiver):
         """ Returns the communication time to transmit the outputs of an """
+        if sender == receiver:
+            return 0
         nbytes = sum(map(bytes, an.outputs))
         intercept, slope = cdict[sender, receiver]
         return slope*nbytes + intercept
