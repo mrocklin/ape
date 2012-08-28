5359274d80b394bd4498ce9a64697d215312f8b0
Matthew Rocklin <mrocklin@gmail.com>
2012-08-28 09:48:43 -0500

    Clean template.py
    Add debug printing messages
    
    Add ape imports
    
    import array from numpy

--- ape/template.py
+++ ape/template.py
@@ -1,8 +1,9 @@
-from mpi_prelude import *
+from ape.mpi_prelude import *
 import theano
+from numpy import array
 
 # Unpack envs/jobs from file
-from env_manip import unpack_many
+from ape.env_manip import unpack_many
 env_file = open("%(env_filename)s", 'r')
 envs = unpack_many(env_file)
 env_file.close()
@@ -11,14 +12,20 @@ env_file.close()
 mode = theano.compile.mode.get_default_mode()
 %(compile)s
 
+print "Compilation on "+host+" finished"
+
 # Initialize variables
 %(variable_initialization)s
+
+print "Initialization on "+host+" finished"
 
 # Non-blocking receives
 %(recv)s
 
 # Wait for everyone to finish compiling and setting up receives
 comm.barrier()
+
+print "Computation on "+host+" begun"
 
 # Compute
 %(compute)s
