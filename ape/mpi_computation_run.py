4de0ae28715e2b5ea054eb3549b22b272467a1f4
Matthew Rocklin <mrocklin@gmail.com>
2012-08-31 12:58:33 -0500

    factor out timing in mpi_computation_run

--- ape/mpi_computation_run.py
+++ ape/mpi_computation_run.py
@@ -1,16 +1,30 @@
 # See companion file mpi_computation_time.py
+import time
+from sys import argv, stdout, stdin, stderr
+import theano
+
 
 def debugprint(s):
     pass
     stderr.write(str(s)+"\n")
 
+def time_computation(inputs, outputs, numeric_inputs, niter):
+
+    f = theano.function(inputs, outputs)
+
+    starttime = time.time()
+    debugprint("Computing")
+    for n in xrange(niter):
+        outputs = f(*numeric_inputs)
+    endtime = time.time()
+    duration = endtime - starttime
+
+    return duration/niter
+
 if __name__ == '__main__':
-    import time
-    from sys import argv, stdout, stdin, stderr
     import ast
     import numpy as np
     from ape.env_manip import unpack_many
-    import theano
     from theano.tensor.utils import shape_of_variables
 
     # Setup
@@ -28,21 +42,14 @@ if __name__ == '__main__':
 
         inputs = filter(lambda x: not isinstance(x, theano.Constant),
                         fgraph.inputs)
+        outputs = fgraph.outputs
         # Compile and instantiate
         num_inputs = [np.asarray(np.random.rand(*known_shapes[str(var)])).astype(var.dtype)
                              for var in inputs]
 
-        f = theano.function(inputs, fgraph.outputs)
-        starttime = time.time()
+        duration = time_computation(inputs, outputs, num_inputs, niter)
 
-        debugprint("Computing")
-        for n in xrange(niter):
-            outputs = f(*num_inputs)
-
-        endtime = time.time()
-        duration = endtime - starttime
-
-        results.append(duration / niter)
+        results.append(duration)
 
     stdout.write(str(results))
     stdout.close()
---
+++ ape/test/test_mpi_computation_run.py
@@ -0,0 +1,14 @@
+from ape.mpi_computation_run import time_computation
+import theano
+import numpy as np
+
+def test_time_computaiton():
+    x = theano.tensor.matrix('x')
+    y = theano.tensor.matrix('y')
+    z = theano.tensor.dot(x, y)
+
+    xx = np.ones((100, 100), dtype=x.dtype)
+    yy = np.ones((100, 100), dtype=y.dtype)
+
+    time = time_computation((x, y), (z,), (xx, yy), 5)
+    assert isinstance(time, float)
