51920d638c6c42f65b53e0fa2202155e44aa5216
Matthew Rocklin <mrocklin@gmail.com>
2012-08-31 12:59:12 -0500

    add remote test or mpi_computation_time

--- ape/test/test_mpi_computation_time.py
+++ ape/test/test_mpi_computation_time.py
@@ -2,7 +2,7 @@ from ape.mpi_computation_time import compute_time_on_machine
 from ape.env_manip import fgraph_iter, variables_with_names
 import theano
 
-def test_compute_time_on_machine():
+def _test_compute_time_on_machine(machine):
     x = theano.tensor.matrix('x')
     y = theano.tensor.matrix('y')
     z = theano.tensor.dot(x+1, 2*y)
@@ -10,9 +10,14 @@ def test_compute_time_on_machine():
     fgraph = theano.FunctionGraph((x,y), (z,))
     input_shapes = {x:(1000,1000), y:(1000,1000)}
     niter = 3
-    machine = 'ankaa.cs.uchicago.edu'
 
     times = compute_time_on_machine(fgraph, input_shapes, machine, niter)
     assert isinstance(times, dict)
     assert set(map(str, fgraph.nodes)) == set(times.keys())
     assert all(isinstance(val, float) for val in times.values())
+
+def test_nfs():
+    _test_compute_time_on_machine('ankaa.cs.uchicago.edu')
+
+def test_remote():
+    _test_compute_time_on_machine('baconost.cs.uchicago.edu')
