bf3e46e2739ca88fc47a99712617d851da382bb6
Matthew Rocklin <mrocklin@gmail.com>
2012-09-01 12:23:29 -0500

    fix imports in master example

--- ape/examples/master.py
+++ ape/examples/master.py
@@ -1,16 +1,16 @@
 from theano.tensor.utils import shape_of_variables
 from theano.gof.fg import FunctionGraph as FunctionGraph
-from ape.timings.comptime import comptime_dict
-from ape.timings.commtime import compute_commtimes
+from ape.timings.comptime_mpi import comptime_dict
+from ape.timings.commtime_mpi import commtime_dict_mpi
 from ape.env_manip import variables_with_names, math_optimize
 from ape.util import save_dict, load_dict
-from ape.timings import (make_runtime_function, make_commtime_function)
+from ape.timings.comptime import make_runtime_function
+from ape.timings.commtime import make_commtime_function
 from ape.theano_to_milp import dummy_ability
 from ape.master import compile
 
-from nfs_triple import machine_groups
+from nfs_triple import machine_groups, network, machines
 from kalman import inputs, outputs, input_shapes
-machines = sum(machine_groups, ())
 
 variables_with_names(inputs, outputs) # give identifiers to all variables
 fgraph = FunctionGraph(inputs, outputs)
@@ -29,8 +29,8 @@ save_dict('compute_times.dat', compute_times)
 compute_cost = make_runtime_function(compute_times)
 
 # Communication Cost
-comm_dict = compute_commtimes([10,100,1000,2000]*5,
-            set(machines))
+comm_dict = commtime_dict_mpi(network, [10,100,1000,2000]*5)
+
 save_dict('comm_times.dat', comm_dict)
 # comm_dict = load_dict('comm_times.dat')
 comm_cost = make_commtime_function(comm_dict, all_shapes)
