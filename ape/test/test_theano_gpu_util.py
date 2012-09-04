f41e48d6cff147225b120fc08aa827a8e95c8b30
Matthew Rocklin <mrocklin@gmail.com>
2012-09-04 09:49:51 -0500

    add cpu-gpu comm utility functions

--- ape/test/test_theano_gpu_util.py
+++ ape/test/test_theano_gpu_util.py
@@ -1,4 +1,6 @@
 from ape.theano_gpu_util import *
+import numpy as np
+import theano
 
 def test_cpu_to_gpu_graph():
     x = theano.tensor.matrix('x')
@@ -10,3 +12,19 @@ def test_cpu_to_gpu_graph():
     f = theano.function(gpu_inputs, gpu_outputs)
     assert all(isinstance(inp, theano.sandbox.cuda.CudaNdarrayVariable)
                 for inp in (gpu_inputs+gpu_outputs))
+
+def test_togpu_tocpu_data():
+    x = np.ones((5,5), dtype='float32')
+    gx = togpu_data(x)
+    assert isinstance(gx, theano.sandbox.cuda.CudaNdarray)
+    xx = tocpu_data(gx)
+    assert (x==xx).all()
+
+
+def test_cpu_to_gpu_var():
+    x = theano.tensor.matrix('x')
+    gx, cx = cpu_to_gpu_var(x)
+    f = theano.function((gx,), cx)
+    xx = np.ones((5,5), dtype='float32')
+    gxx = togpu_data(xx)
+    assert (f(gxx) == xx).all()
--- ape/theano_gpu_util.py
+++ ape/theano_gpu_util.py
@@ -1,10 +1,18 @@
 # Utility functions for dealing with GPU variables in Theano
 from theano.sandbox import cuda
+import numpy as np
 import theano
 
 def cpu_to_gpu_var(x):
     type = cuda.CudaNdarrayType(broadcastable=x.broadcastable)
     name = 'gpu_%s'%x.name
+    gpu_var = cuda.CudaNdarrayVariable(type=type, name=name)
+    cpu_var = cuda.host_from_gpu(gpu_var)
+    return gpu_var, cpu_var
+
+def gpu_to_cpu_var(x):
+    type = cuda.CudaNdarrayType(broadcastable=x.broadcastable)
+    name = 'cpu_%s'%x.name
     gpu_var = cuda.CudaNdarrayVariable(type=type, name=name)
     cpu_var = cuda.host_from_gpu(gpu_var)
     return gpu_var, cpu_var
@@ -28,3 +36,60 @@ def cpu_to_gpu_graph(inputs, outputs):
     final_outputs = map(lambda o: theano.Out(o, borrow=True), gpu_outputs)
 
     return tuple(gpu_inputs), tuple(gpu_outputs)
+
+def togpu_data(x, copy=True):
+    """ Sends a cpu variable to the GPU
+
+    Works for numpy.ndarrays
+
+    >>> x = np.ones((5,5), dtype='float32')
+    >>> togpu_data(x)
+    <CudaNdarray at 0x2ece870>
+
+    See also
+    --------
+        tocpu_data
+    """
+
+    if isinstance(x, np.ndarray):
+        return theano.sandbox.cuda.shared_constructor(x).get_value(
+                borrow=True, return_internal_type=True)
+    if isinstance(x, theano.sandbox.cuda.CudaNdarray):
+        if copy:
+            return x.copy()
+        else:
+            return x
+    if isinstance(x, theano.sandbox.cuda.var.CudaNdarraySharedVariable):
+        return xg.get_value(return_internal_type=True, borrow=copy)
+    assert False
+
+def tocpu_data(x, copy=True):
+    """ Sends a GPU variable to the CPU
+
+    Works for theano.sandbox.cuda.CudaNdarrays
+
+    >>> x  = np.ones((5,5), dtype='float32')
+    >>> gx = togpu_data(x)
+    >>> xx = tocpu_data(gx)
+    >>> xx
+    array([[ 1.,  1.,  1.,  1.,  1.],
+           [ 1.,  1.,  1.,  1.,  1.],
+           [ 1.,  1.,  1.,  1.,  1.],
+           [ 1.,  1.,  1.,  1.,  1.],
+           [ 1.,  1.,  1.,  1.,  1.]], dtype=float32)
+
+    See also
+    --------
+        tocpu_data
+    """
+
+    if isinstance(x, theano.sandbox.cuda.CudaNdarray):
+        return np.asarray(x)
+    if isinstance(x, np.ndarray):
+        if copy:
+            return x.copy()
+        else:
+            return x
+    if isinstance(x, theano.sandbox.cuda.var.CudaNdarraySharedVariable):
+        return x.get_value(return_internal_type=False)
+    assert False
