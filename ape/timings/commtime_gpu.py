3434ed12ef2e21e14aa10c724bae370d256d19d3
Matthew Rocklin <mrocklin@gmail.com>
2012-09-04 11:08:44 -0500

    commtime_dict_to/from gpu functions work

--- ape/timings/commtime_gpu.py
+++ ape/timings/commtime_gpu.py
@@ -1,12 +1,54 @@
+from ape.timings.commtime_util import run_on_hosts, model_from_values
+from ape.util import merge
+from ape import ape_dir
 
 def commtime_dict_togpu(network, nbytes=[10, 100, 1000, 10000]):
     if not all(v['type'] == 'togpu' for v in network.values()):
         raise ValueError("Trying to compute network properties of incompatible"
                          "network - must contain only togpu connections")
-    raise NotImplementedError()
+
+
+    return {(host, gpu):
+                merge(network[host, gpu],
+                      model_from_values(comm_times_togpu(nbytes, host)))
+            for host, gpu in network
+            if network[host, gpu]['type'] == 'togpu'}
 
 def commtime_dict_fromgpu(network, nbytes=[10, 100, 1000, 10000]):
     if not all(v['type'] == 'fromgpu' for v in network.values()):
         raise ValueError("Trying to compute network properties of incompatible"
-                         "network - must contain only fromgpu connections")
-    raise NotImplementedError()
+                         "network - must contain only togpu connections")
+
+
+    return {(gpu, host):
+            merge(network[gpu, host],
+                  model_from_values(comm_times_fromgpu(nbytes, host)))
+            for gpu, host in network
+            if network[gpu, host]['type'] == 'fromgpu'}
+
+def comm_times_togpu(nbytes, host):
+    """ Computes transit times between host and gpu
+    Returns a list of [(nbytes, transit-time)]
+    >>> comm_times([10, 100, 1000], 'host.univ.edu')
+     [(40, 0.00015091896057128906),
+     (400, 0.0002040863037109375),
+     (4000, 0.0005209445953369141)]
+    """
+
+    return run_on_hosts((host, ),
+       '''python %sape/timings/commtime_togpu_run.py "%s"'''%(
+           ape_dir, str(nbytes)))
+def comm_times_fromgpu(nbytes, host):
+    """ Computes transit times between gpu and host
+    Returns a list of [(nbytes, transit-time)]
+    >>> comm_times([10, 100, 1000], 'host.univ.edu')
+     [(40, 0.00015091896057128906),
+     (400, 0.0002040863037109375),
+     (4000, 0.0005209445953369141)]
+    """
+
+    return run_on_hosts((host, ),
+     '''python %sape/timings/commtime_tocpu_run.py "%s"'''%(
+         ape_dir, str(nbytes)))
+
+
--- ape/timings/tests/test_commtime_gpu.py
+++ ape/timings/tests/test_commtime_gpu.py
@@ -2,6 +2,7 @@ import ast
 import os
 from ape import ape_dir
 import theano
+from ape.timings.commtime_gpu import commtime_dict_togpu, commtime_dict_fromgpu
 
 def test_commtime_tocpu_run():
     if not theano.sandbox.cuda.cuda_available:
@@ -22,3 +23,16 @@ def test_commtime_togpu_run():
     assert isinstance(results, list)
     assert all(len(result) == 2 for result in results)
     assert len(results) == len(ns)
+
+def test_commtime_dict_togpu():
+    network = {('baconost.cs.uchicago.edu', 'baconost.cs.uchicago.edu_gpu'):
+                     {'type':'togpu'}}
+    result = commtime_dict_togpu(network)
+    assert isinstance(result[network.keys()[0]]['intercept'], float)
+
+def test_commtime_dict_fromgpu():
+    network = {('baconost.cs.uchicago.edu_gpu', 'baconost.cs.uchicago.edu'):
+                     {'type':'fromgpu'}}
+    result = commtime_dict_fromgpu(network)
+    assert isinstance(result[network.keys()[0]]['intercept'], float)
+
