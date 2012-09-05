3bc6b0c2ab443e51254af2cb556b15572e9d5093
Matthew Rocklin <mrocklin@gmail.com>
2012-09-05 10:33:29 -0500

    fix test - timings tests pass

--- ape/timings/tests/test_comptime_gpu.py
+++ ape/timings/tests/test_comptime_gpu.py
@@ -4,5 +4,5 @@ from test_comptime_xpu import _test_comptime_dict_xpu
 def test_comptime_dict_gpu():
     machines = {'baconostgpu': {'type':'gpu',
                                 'host':'baconost.cs.uchicago.edu'}}
-    machine_groups = (('baconostgpu', ))
+    machine_groups = (('baconostgpu', ),)
     _test_comptime_dict_xpu(machines, machine_groups, comptime_dict_gpu)
