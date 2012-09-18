8cfaf22478833ce146692c6d511a4dd78dac5e07
Matthew Rocklin <mrocklin@gmail.com>
2012-09-18 11:24:00 -0500

    timing.__init__ documentation and direct imports

--- ape/timings/__init__.py
+++ ape/timings/__init__.py
@@ -1,4 +1,19 @@
+""" Estimate computation and communication runtimes by direct execution
+
+See also:
+    communication       - module for network communication
+    commtime_dict       - converts network discription to timing dict
+    make_commtime_fn    - converts timing dict to callable function
+
+    computation         - module for computation times
+    comptime_dict       - converts fgraphs and list of machines to timing dicts
+    make_comptime_fn    - converts timing dict to callable function
+"""
+
 import communication
 import computation
 import util
 import theano_gpu_util
+
+from communication import commtime_dict, make_commtime_fn
+from computation   import comptime_dict, make_comptime_fn
