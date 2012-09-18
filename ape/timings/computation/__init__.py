7d174c04ebc27791ce726c765ca6afdfde01d446
Matthew Rocklin <mrocklin@gmail.com>
2012-09-18 11:14:28 -0500

    docstrings for computations

--- ape/timings/computation/__init__.py
+++ ape/timings/computation/__init__.py
@@ -1,1 +1,7 @@
+""" Estimate runtimes by direct execution. Depends on Theano.
+
+See Also:
+    comptime_dict
+    make_runtime_function
+"""
 from master import comptime_dict, make_runtime_function
--- ape/timings/computation/master.py
+++ ape/timings/computation/master.py
@@ -5,6 +5,23 @@ from ape.util import merge
 comptime_dict_fns = [comptime_dict_cpu, comptime_dict_gpu]
 
 def comptime_dict(fgraph, input_shapes, niter, machines, machine_groups=None):
+    """ Estimate record average computation times of tasks in a graph
+
+    inputs:
+        fgraph          - a theano.FunctionGraph describing the computation
+        input_shapes    - a dict {var: (shape)} for each input variable
+        niter           - the number of times to run each computation
+        machines        - a list of machines on which to run each computation
+        machine_groups  - an iterable of sets of identical machines
+                        - only a representative of each set will be used
+
+    outputs:
+        A dict mapping {{set-of-identical-machines}: {apply-node : runtime}}
+
+    See Also:
+        make_runtime_function   - converts the output of this function into a
+                                - callable function
+    """
     dicts = (fn(fgraph, input_shapes, niter, machines, machine_groups)
              for fn in comptime_dict_fns)
     return merge(*dicts)
@@ -13,11 +30,12 @@ def make_runtime_function(machine_time_dict):
     """
     Create a callable function from a dict containing runtimes for machines
 
-    inputs
-    ------
-    machine_time_dict : Dict mapping set of machines to times Dict
-        each times dict maps string representation of an apply node to a time
-        example:
+    inputs:
+        machine_time_dict - dict mapping set of machines to times Dict
+            each times dict maps string representation of an apply node to a
+            time
+    example-input:
+
     >>> {('ankaa', 'mimosa'):
             {'Elemwise{add,no_inplace}(x, Elemwise{mul,no_inplace}.0)': 0.01074,
              'Elemwise{mul,no_inplace}(x, x)': 0.005693912506103516},
@@ -25,10 +43,9 @@ def make_runtime_function(machine_time_dict):
             {'Elemwise{add,no_inplace}(x, Elemwise{mul,no_inplace}.0)':0.01074,
              'Elemwise{mul,no_inplace}(x, x)': 0.005693912506103516}}
 
-    outputs
-    -------
-    A callable function that takes a machine-id (like 'ankaa') and an apply
-    node and returns a time
+    output:
+        A callable function that takes a machine-id (like 'ankaa') and an apply
+        node and returns a time
     """
     # Break out the ids to a flat dict
     d = {id:machine_time_dict[ids] for ids in machine_time_dict for id in ids}
