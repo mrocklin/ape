631e874c30b51cf95bfbe440a061915b0cf175ea
Matthew Rocklin <mrocklin@gmail.com>
2012-09-25 13:01:58 -0500

    add ape version of shape_of_variables

--- ape/test/test_theano_util.py
+++ ape/test/test_theano_util.py
@@ -1,4 +1,16 @@
-from ape.theano_util import *
+from ape.theano_util import bytes_of_dtype, shape_of_variables
+import numpy as np
+import theano
 
 def test_bytes_of_dtype():
     assert bytes_of_dtype('float32') == 4
+
+def test_shape_of_variables():
+    x = theano.tensor.matrix('x')
+    y = x[1:, 2:]
+    known_shapes = shape_of_variables((x,), (y,), {x: (10, 10)})
+    assert known_shapes[y] == (9, 8)
+    assert not isinstance(known_shapes[y][0], np.ndarray)
+
+    # just make sure that we can do this afterwards
+    fgraph = theano.FunctionGraph((x,), (y,))
--- ape/theano_util.py
+++ ape/theano_util.py
@@ -1,7 +1,16 @@
 import theano
+from ape.util import dearrayify
 
 known_dtypes = {'float32': 4, 'float64':8, 'int32':4, 'int64':8}
 def bytes_of_dtype(dtype):
     if dtype in known_dtypes:
         return known_dtypes[dtype]
     raise NotImplementedError("Don't know how many bytes are in dtype%s"%dtype)
+
+# This should be moved upstream
+def shape_of_variables(i, o, input_shapes):
+    fgraph = theano.FunctionGraph(i, o)
+    known_shapes = dearrayify(
+            theano.tensor.utils.shape_of_variables(fgraph, input_shapes))
+    fgraph.disown()
+    return known_shapes
