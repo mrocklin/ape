29f9b05fd8cb8020c73336722eaa91855eec9f00
Matthew Rocklin <mrocklin@gmail.com>
2012-09-28 11:44:53 -0500

    add gpu comparators to codegen/run.py

--- ape/codegen/run.py
+++ ape/codegen/run.py
@@ -15,7 +15,16 @@ from theano.gof.sched import sort_schedule_fn
 from theano.tensor.io import mpi_cmps
 
 sched_cmp = sched_to_cmp(read_sched(filename_root+".sched"))
-scheduler = sort_schedule_fn(sched_cmp, *mpi_cmps)
+
+# Gather gpu comparators if we have a GPU
+if theano.config.device == 'gpu':
+    from theano.sandbox.cuda.async import gpu_cmps
+    gpu_sched_cmp = sched_to_cmp(read_sched(filename_root+"-gpu.sched"))
+    gpu_cmps = (gpu_sched_cmp,) + gpu_cmps
+else:
+    gpu_cmps = ()
+
+scheduler = sort_schedule_fn(sched_cmp, *(gpu_cmps + mpi_cmps))
 linker = theano.OpWiseCLinker(schedule=scheduler)
 mode = theano.Mode(linker=linker, optimizer=None)
 
