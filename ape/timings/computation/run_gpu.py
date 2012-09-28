583aeef07f6dbc21afbdf392a45a975a387da5c3
Matthew Rocklin <mrocklin@gmail.com>
2012-09-28 11:42:32 -0500

    return high time value if op can not run on gpu

--- ape/timings/computation/run_gpu.py
+++ ape/timings/computation/run_gpu.py
@@ -7,6 +7,12 @@ from ape.theano_gpu_util import cpu_to_gpu_graph, togpu_data
 def time_computation(inputs, outputs, numeric_inputs, niter):
 
     gpu_inputs, gpu_outputs = cpu_to_gpu_graph(inputs, outputs)
+    # TODO: replace this with a can_run_on(job, machine) function
+    #       use this function in tompkins
+    if not all(isinstance(n.op, theano.sandbox.cuda.GpuOp)
+            for n in theano.gof.graph.list_of_nodes(gpu_inputs, gpu_outputs)):
+        return 99999.9
+
     gpu_numeric_inputs = map(togpu_data, numeric_inputs)
 
     f = theano.function(gpu_inputs, gpu_outputs)
